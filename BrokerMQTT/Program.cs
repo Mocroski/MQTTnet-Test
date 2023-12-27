c72a87c524a27ad4c59cf52e59d8144ade92aa33
unknown <devmocroski@gmail.com>
2023-12-27 20:15:37 -0300

    First Commit

---
+++ BrokerMQTT/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Server;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var options = new MqttServerOptionsBuilder()
+            .WithConnectionBacklog(100)
+            .WithDefaultEndpointPort(1883)
+            .Build();
+
+        var mqttServer = new MqttFactory().CreateMqttServer();
+
+        mqttServer.ClientConnectedHandler = new MqttServerClientConnectedHandlerDelegate(e =>
+        {
+            Console.WriteLine($"Gay conectado: {e.ClientId}");
+        });
+
+        mqttServer.ClientDisconnectedHandler = new MqttServerClientDisconnectedHandlerDelegate(e =>
+        {
+            Console.WriteLine($"Gay desconectado: {e.ClientId}");
+        });
+
+        mqttServer.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(e =>
+        {
+            Console.WriteLine($"Mensagem recebida no tópico: {e.ApplicationMessage.Topic}");
+            Console.WriteLine($"Conteúdo da mensagem: {System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+        });
+
+        await mqttServer.StartAsync(options);
+
+        Console.WriteLine("Servidor MQTT iniciado. Pressione Enter para encerrar.");
+        Console.ReadLine();
+
+        await mqttServer.StopAsync();
+    }
+}
---
+++ ClientMQTT/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new MqttFactory();
+        var mqttClient = factory.CreateMqttClient();
+
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883) 
+            .WithClientId("123456") 
+            .Build();
+
+        mqttClient.UseConnectedHandler(e =>
+        {
+            Console.WriteLine("Conectado ao servidor MQTT!");
+        });
+
+        mqttClient.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("Desconectado do servidor MQTT!");
+        });
+
+        mqttClient.UseApplicationMessageReceivedHandler(e =>
+        {
+            Console.WriteLine($"Mensagem recebida do tópico: {e.ApplicationMessage.Topic}");
+            Console.WriteLine($"Conteúdo da mensagem: {System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+        });
+
+        await mqttClient.ConnectAsync(options);
+
+        Console.WriteLine("GayzinhoSigiloso conectado ao servidor MQTT de Sexo!");
+        Console.ReadLine();
+
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("topic/sigilo") 
+            .WithPayload("Quero da ocu") 
+            .WithExactlyOnceQoS()
+            .WithRetainFlag()
+            .Build();
+
+        await mqttClient.PublishAsync(message);
+
+        Console.WriteLine("Mensagem enviada. Pressione Enter para sair.");
+        Console.ReadLine();
+
+        await mqttClient.DisconnectAsync();
+    }
+}
